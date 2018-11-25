dbd3e644325033e9471db4295470d3f601062f98
nahomiyoz <nahomiyoz@gmail.com>
2018-11-25 22:27:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,4 @@ function shout(string)
 {
   return string.toUpperCase()
 }
-function whisper(string){
-  return string.toLowerCase()
-}
-function logwhisper(string){
-  return string.toUpperCase()
-}
-function logShout(string){
-  return string.toUpperCase()
-}
+
